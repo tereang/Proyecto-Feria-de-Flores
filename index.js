4c4652d7527b9494844353911d9f70dd924ee6fe
tereang <you@example.com>
2023-03-28 17:13:58 -0300

    leyendo json ok

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require ("express")
 const hbs = require ("hbs")
+const fs = require ("fs")
 
 hbs.registerPartials(__dirname +"/views/partials")
 
 const app = express()
 const port = 3000
+
 
 app.use(express.static(__dirname + '/public'))
 app.set('views engine','hbs')
@@ -13,14 +15,22 @@ app.set('views engine','hbs')
 
 //iniciar express y handlebars
 app.get ('/', function(req, res){
-  res.render("index.hbs")
+  fs.readFile("./ramo.json", 'utf8', function(err, data){
+  const ramos = JSON.parse(data)
+ res.render("index.hbs",ramos)
 })
 
 app.get ('/datos', function(req, res){
-  fetch("ramo.json").then(data => data.json()) .then(result => res.render (result) )})
+  fetch("C:\Javascript\tareas-pasadas\Proyecto-Feria-de-Flores\ramo.json",{ 
+    headers: { 'Content-Type': "application/json"},
+  })
+  .then(data => data.json()) 
+  .then(result => console.log(result) )
+  res.send("corriendo")
+})
+})
 
-  app.listen(port, () => {
-console.log  (`servidor en puerto ${port}`)
-}
-)
+app.listen(port, () => {
+  console.log  (`servidor en puerto ${port}`)
+})
 
