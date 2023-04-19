0a584e1c9cc95814df652d880e0e5a951f576676
tereang <you@example.com>
2023-04-18 23:47:54 -0400

    se crean cards para texto  e imagenes se cambia boton consulta

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require ("express")
 const hbs = require ("hbs")
 const fs = require ("fs")
 const { error } = require("console")
+const router = express.Router();							
+//const db = require('../db');			
 
 hbs.registerPartials(__dirname +"/views/partials")
 
@@ -32,25 +34,24 @@ app.set('views engine','hbs')
 
 app.get ('/', async function(req, res){
 
-  
-  const results = await pool.query('SELECT * FROM ramo where stock >0 order by precio asc');
-  const ramos = {ramo:results.rows} 
+  const resultado = await fetch("http://localhost:4000/api/v1/ramos");
+  const data = await resultado.json();
+  //const results = await pool.query('SELECT * FROM ramo where stock >0 order by precio asc');
+  const ramos = {ramo:data} 
   res.render("index.hbs",ramos) 
   //console.table(results.rows);
 });
 
 //boton para ver stock en ramo
-const express = require('express');							
-const router = express.Router();							
-const db = require('../db');							
-							
+
+				
 router.get('/', (req, res) => {							
-    db.query('SELECT * FROM ramo WHERE stock > 0 ORDER BY precio ASC', (err, results) => {							
+    pool.query('SELECT * FROM ramo WHERE stock > 0 ORDER BY precio ASC', (err, results) => {							
         if (err) {							
             console.log(err);							
             res.status(500).send('Error al obtener los datos');							
         } else {							
-            res.render('index', {ramos: results});							
+            res.render('index.hbs', {ramos: results});							
         }							
     });							
 });							
